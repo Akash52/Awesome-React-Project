df08a706c8bbf93814be38989bb2af40e8484257
Akash52 <ac8572611@gmail.com>
2020-10-07 08:57:54 +0530

    Text to Speech

---
+++ Text-to-speech-react-app/text-to-speech-react-app-gh-pages/precache-manifest.b35e28e5160e1ea74281bdca10681ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2268982723cfffa5b1d942fdd21e2ecd",
+    "url": "/text-to-speech-react-app/index.html"
+  },
+  {
+    "revision": "42c54aaa9359dbd3b575",
+    "url": "/text-to-speech-react-app/static/css/main.3525cb7c.chunk.css"
+  },
+  {
+    "revision": "054bb7e7f2da9881b310",
+    "url": "/text-to-speech-react-app/static/js/2.0df5abde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-to-speech-react-app/static/js/2.0df5abde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c54aaa9359dbd3b575",
+    "url": "/text-to-speech-react-app/static/js/main.6a7a74cd.chunk.js"
+  },
+  {
+    "revision": "6699de67163b95c1ab9f",
+    "url": "/text-to-speech-react-app/static/js/runtime-main.ec977d0b.js"
+  }
+]);
