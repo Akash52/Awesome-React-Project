18ff457545c3770e166198c0ec46e0387671384c
Akash52 <ac8572611@gmail.com>
2020-10-07 09:12:47 +0530

    Gallery

---
+++ React-gallery-master/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'bootswatch/dist/materia/bootstrap.min.css';
+import axios from 'axios';
+import Search from "./Search";
+
+function App() {
+  return (
+    
+      <div className="container mt-5">
+        <Search/>
+      </div>
+     
+    
+  );
+}
+
+export default App;
